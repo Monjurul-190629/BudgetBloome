4380525ee411fb174b1d2da7cbe851292452d43b
monjurul-190629 <monjurulalam190629@gmail.com>
2026-04-11 23:45:42 +0600

    feat: dotenv added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,9 @@
 import http from "http";
 import app from "./app";
 import { Server } from "socket.io";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const server = http.createServer(app);
 
@@ -11,7 +14,19 @@ const io = new Server(server, {
   },
 });
 
-const PORT = 5000;
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("message", (data) => {
+    console.log("Message:", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 8000
 
 server.listen(PORT, () => {
   console.log(` Server is running on http://localhost:${PORT}`);
