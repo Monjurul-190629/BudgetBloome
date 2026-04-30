8ed87ac87e5d4f07c830f221a8b66ee48f3fb37d
monjurul-190629 <monjurulalam190629@gmail.com>
2026-04-30 20:54:37 +0600

    fix: dashboard header

--- frontend/src/components/layout/DashboardHeader.tsx
+++ frontend/src/components/layout/DashboardHeader.tsx
@@ -6,9 +6,9 @@ const DashboardHeader = ({ title }: Props) => {
   return (
     <div className="w-full flex flex-col gap-1">
       <h1 className="text-xl sm:text-2xl font-semibold ">{title}</h1>
-      <p className="text-[14px]">
-        Welcome back manage your{" "}
-        <span className="font-medium ">{title.toLowerCase()}</span> here
+      <p className="text-[14px] text-gray-300 dark:text-gray-500">
+        Welcome to <span className="font-medium ">{title.toLowerCase()}</span>{" "}
+        management
       </p>
     </div>
   );
