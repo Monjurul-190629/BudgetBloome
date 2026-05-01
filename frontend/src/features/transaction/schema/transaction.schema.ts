16133fd55a1041bd5a838aea4bddc47c4dda4747
monjurul-190629 <monjurulalam190629@gmail.com>
2026-05-01 20:17:07 +0600

    fix: transaction schema with enum

--- frontend/src/features/transaction/schema/transaction.schema.ts
+++ frontend/src/features/transaction/schema/transaction.schema.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
 
+export const transactionTypeEnum = z.enum(["income", "expense"])
+
 export const transactionSchema = z.object({
   wallet: z.string().min(1, "Wallet is required"),
   category_name: z.string().min(1, "Category name is required"),
   description: z.string().optional(),
+  type: transactionTypeEnum,
   amount: z.coerce
     .number({
       message: "Amount is required",
